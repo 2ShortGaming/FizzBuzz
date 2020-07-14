e9a634f9cd5e4ef34d8291072ad63a4587112823
2ShortGaming <66035017+2ShortGaming@users.noreply.github.com>
2020-07-14 16:31:05 -0400

    updates
    added js file and moved all script to js file

---
+++ FizzBuzz/js/main.js
@@ -0,0 +1,34 @@
+﻿$("#btnFB").on("click", function () {
+    let fizzNum = parseInt($("#fbInput1").val());
+    let buzzNum = parseInt($("#fbInput2").val());
+
+    let fizzMod = 0;
+    let buzzMod = 0;
+    let output = new Array();
+
+    for (let loop = 1; loop <= 100; loop++) {
+        fizzMod = loop % fizzNum;
+        buzzMod = loop % buzzNum;
+
+        if (fizzMod == 0 && buzzMod == 0) {
+            output.push(`${loop}: <span class = 'fizzbuzz'> FizzBuzz</span>`)
+        }
+        else if (fizzMod == 0) {
+            output.push(`${loop}: <span class = 'fizz'> Fizz</span>`)
+        }
+        else if (buzzMod == 0) {
+            output.push(`${loop}: <span class = 'buzz'> Buzz</span>`)
+        }
+        else {
+            output.push(`${loop}: none`);
+        }
+
+    }
+    $("#fbOut").html(output.join(`<span class = 'divide'> | </span>`));
+
+});
+
+$("#btnClear").on("click", function () {
+    $("#fbInput1, #fbInput2").val("");
+    $("#fbOut").text("");
+})
